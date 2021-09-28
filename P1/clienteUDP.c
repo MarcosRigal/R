c378334114bad2466e348054dcae6619132cd1b8
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-09-28 13:26:24 +0200

    Almost done

--- P1/clienteUDP.c
+++ P1/clienteUDP.c
@@ -10,82 +10,79 @@
 
 /* --------------------------------------------------------------------------------------
  
- Envía un número aleatorio al servidor, quien el devuelve el número incrementado
+ Envï¿½a un nï¿½mero aleatorio al servidor, quien el devuelve el nï¿½mero incrementado
 
 ---------------------------------------------------------------------------------------- */
- 
-int main ( )
+
+int main(int argc, char const *argv[])
 {
-  
-		/*---------------------------------------------------- 
+
+	/*---------------------------------------------------- 
 			Descriptor del socket y buffer para datos 
 		-----------------------------------------------------*/
-		int Socket_Cliente;
-		int Datos;
-   
-   	/* -----------------------------------------------------
+	int Socket_Cliente;
+	char Cadena[256];
+
+	/* -----------------------------------------------------
    		Informaci\ufffdn del Servidor
    	-----------------------------------------------------*/
-   	struct sockaddr_in Servidor;  
-   	socklen_t Longitud_Servidor;
-  
-  
-   	/* --------------------------------------------------
+	struct sockaddr_in Servidor;
+	socklen_t Longitud_Servidor;
+
+	/* --------------------------------------------------
 			Se abre el socket cliente
 		---------------------------------------------------*/
-		Socket_Cliente = socket (AF_INET, SOCK_DGRAM, 0);
-		if (Socket_Cliente == -1)
-		{
-			printf ("No se puede abrir el socket cliente\n");
-    			exit (-1);	
-		}
+	Socket_Cliente = socket(AF_INET, SOCK_DGRAM, 0);
+	if (Socket_Cliente == -1)
+	{
+		printf("No se puede abrir el socket cliente\n");
+		exit(-1);
+	}
 
- 		/*---------------------------------------------------------------------
+	/*---------------------------------------------------------------------
 			Necesitamos una estructura con la informaci\ufffdn del Servidor
 			para poder solicitarle un servicio.
    		----------------------------------------------------------------------*/
-		Servidor.sin_family = AF_INET;
-		Servidor.sin_port = htons(2000);
-		Servidor.sin_addr.s_addr =  inet_addr("127.0.0.1");
-  	 	Longitud_Servidor = sizeof(Servidor);
-	
+	Servidor.sin_family = AF_INET;
+	Servidor.sin_port = htons(2000);
+	Servidor.sin_addr.s_addr = inet_addr(argv[1]);
+	Longitud_Servidor = sizeof(Servidor);
 
-		/*------------------------------------------------------------------------
+	/*------------------------------------------------------------------------
 			Se genera un n\ufffdmero aleatorio, que es el que se le manda al
 			servidor.
 		------------------------------------------------------------------------ */
-   	srand (time(NULL)); /* Semilla para n\ufffdmeros aleatorios */
-   	Datos = rand()%20;  /* Aleatorio entre 0 y 19 */
-   	printf ("Envio %d\n", Datos);
+	srand(time(NULL));	 /* Semilla para n\ufffdmeros aleatorios */
+	scanf("%s", Cadena); /* Aleatorio entre 0 y 19 */
+	printf("Envio %s\n", Cadena);
 
-   	
-		/*-----------------------------------------------------------------------
+	/*-----------------------------------------------------------------------
 			Se env\ufffda mensaje al Servidor
 		-----------------------------------------------------------------------*/
-		int enviado = sendto (Socket_Cliente, (char *) &Datos, sizeof(Datos), 0,
-			(struct sockaddr *) &Servidor, Longitud_Servidor);
-   
-   	if (enviado < 0)
-    	{
-			printf("Error al solicitar el servicio\n");
-		}
-    	else
+	for (size_t i = 0; i < 3; i++)
+	{
+		int enviado = sendto(Socket_Cliente, (char *)&Cadena, sizeof(Cadena), 0, (struct sockaddr *)&Servidor, Longitud_Servidor);
+		if (enviado < 0)
 		{
-   
-		/*----------------------------------------------------------------------
-			Esperamos la respuesta del Servidor
-			----------------------------------------------------------------------- */ 
-			int recibido = recvfrom (Socket_Cliente, (char *)&Datos, sizeof(Datos), 0,
-			(struct sockaddr *) &Servidor, &Longitud_Servidor);
-			
-   		if (recibido > 0)
-      		printf ("Leido %d\n", Datos);
-   		else
-      		printf ("Error al leer del servidor\n");
-		
+			printf("Error al solicitar el servicio\n");
+			sleep(atoi(argv[2]));
 		}
-		
-		close(Socket_Cliente);
+		else
+		{
+			/*----------------------------------------------------------------------
+			Esperamos la respuesta del Servidor
+			----------------------------------------------------------------------- */
+			int recibido = recvfrom(Socket_Cliente, (char *)&Cadena, sizeof(Cadena), 0,
+									(struct sockaddr *)&Servidor, &Longitud_Servidor);
+
+			if (recibido > 0)
+				printf("Leido %s\n", Cadena);
+			else
+				printf("Error al leer del servidor\n");
+		}
+	}
+
+	close(Socket_Cliente);
 
 	return 0;
 }
