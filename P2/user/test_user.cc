222452722e95669e29301ae544c4702e1dd78296
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-10-16 17:46:50 +0200

    build: :building_construction: Implemented User Class

--- P2/user/test_user.cc
+++ P2/user/test_user.cc
@@ -3,7 +3,22 @@
 
 using namespace std;
 
-TEST(User, demo)
+TEST(User, Name)
 {
-    EXPECT_FALSE(false);
+    User user;
+    user.setUserName("Marcos");
+    EXPECT_EQ(strcmp(user.getUserName(), "Marcos"), 0);
+}
+
+TEST(User, Password)
+{
+    User user;
+    user.setUserPassword("1234");
+    EXPECT_EQ(strcmp(user.getUserPassword(), "1234"), 0);
+}
+
+TEST(User, Status)
+{
+    User user;
+    EXPECT_TRUE(user.getUserStatus());
 }
--- P2/user/user.h
+++ P2/user/user.h
@@ -1,10 +1,31 @@
 #ifndef USER_H
 #define USER_H
 
+#include <string.h>
+
 using namespace std;
 
 class User
 {
+private:
+    char name_[250];
+    char password_[250];
+    bool isWaiting_;
+
+public:
+    User() { isWaiting_ = true; }
+
+    inline char *getUserName() { return name_; }
+    inline char *getUserPassword() { return password_; }
+    inline bool getUserStatus() { return isWaiting_; }
+
+    inline void setUserName(char *name) { strncpy(name_, name, 250); }
+    inline void setUserName(const char *name) { strncpy(name_, name, 250); }
+
+    inline void setUserPassword(char *password) { strncpy(password_, password, 250); }
+    inline void setUserPassword(const char *password) { strncpy(password_, password, 250); }
+
+    inline void setUserStatus(bool isWaiting) { isWaiting_ = isWaiting; }
 };
 
 #endif
